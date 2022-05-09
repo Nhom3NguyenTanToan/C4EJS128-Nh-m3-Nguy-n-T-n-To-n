b84eeac354305af9430b0a897a381099ab69415e
Nhom3NguyenTanToan <nguyentoan3042003@gmail.com>
2022-05-09 21:05:31 +0700

    btvnB7

---
+++ B7-Btvn số 2/index.js
@@ -0,0 +1,11 @@
+//  b1
+ let txt1="hello World!";
+let x = txt1.length;
+// b2
+alert(x);
+let txt2="we are \"Viking\"";
+alert(txt2);
+// b3
+let str1 = ' Hello';
+let str2 = 'world!';
+alert( str1 + str2);
---
+++ B7-btvn-số1/index.js
@@ -0,0 +1,40 @@
+// // Khai bao Bien , goi ham thong bao
+// var fullName = 'nguyen tan toan';
+// alert(fullName);
+// var age = (19);
+// alert(age);
+// //comment
+// // boi den + ctrl /
+// // Mot So ham Build-in
+// // 1 . Alert
+// // 2. console.log
+// console.log('Nguyen Tan toan    ');
+// console.log(fullName);
+// // 3. confirm()
+// confirm('Ho cua ban la gi')
+// // 4. Prompt
+// prompt('ho cua ban la gi')
+// // 5. setTimeout(() => {
+    
+// // }, timeout);
+// setTimeout(function() {
+//     alert(age)
+// },1000)
+// // 6.setInterval
+// // setInterval(function() {
+// //     console.log('jsbasic' + Math.random())
+// // },100)
+var firstName,lastName,age;
+firstName=prompt('What is ur First Name?');
+lastName=prompt('What is ur Last Name?')
+alert ( 'Hello' +' '+ firstName + ' ' + lastName );
+age=prompt('Nhập số tuổi của bạn');
+alert ('hello tôi là ' + ' ' + firstName + lastName + ' năm nay tôi ' +'' + age +' '+'tuổi');   
+
+// Toan tu
+// Toan tu so hoc : + - * / ** % 
+// Toan tu gan : = 
+// Toan tu so sanh : > < >= <= ==
+// Toan tu logic if ( a && b >0) {
+//     alert('')
+// }
