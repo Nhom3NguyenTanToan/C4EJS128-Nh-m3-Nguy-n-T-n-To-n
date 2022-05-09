8a60c062d897ae27a1348a33340672b48211d158
Nhom3NguyenTanToan <nguyentoan3042003@gmail.com>
2022-05-09 21:18:37 +0700

    btvn

---
+++ B7-Btvn-số 3/index.js
@@ -0,0 +1,20 @@
+//b1
+let txt = "Hello World!";
+txt = txt.toUpperCase();
+alert(txt); //in hoa
+//b2
+let txt2 = "i can ear bananas all day";
+let x = txt2.slice(10,17);
+alert(x);//in ra chu tu dong 10 > 17
+//b3
+let txt3 = "hello World";
+txt4 = txt3.replace("hello", "wellcome");
+alert(txt4)// thay the hien thi tu hell => wellcome
+//b4
+let txt5 = "Hello World";
+txt5 =txt5.toUpperCase();
+alert(txt5)
+//b5
+let txt6 ="Hello world"
+txt6=txt6.toLowerCase();
+alert(txt6);
