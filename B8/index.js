26f5b701f4bba0dc356aacc4de07daf5060b3f1f
Nhom3NguyenTanToan <nguyentoan3042003@gmail.com>
2022-05-11 19:12:08 +0700

    btvn-b8

---
+++ B8/index.js
@@ -0,0 +1,44 @@
+let yourInput = number(prompt("input"))
+console.log("__for__")
+for (let i = 0; i < yourInput; i++) {
+    console.log(`vitri thu: ${
+        i+1
+    }`);
+
+}
+console.log("___while");
+let i = 0;
+let index = 0;
+do {
+console.log(' vi tri thu : ${i + 1');
+index++;
+}while (
+i < yourInput
+)
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+
+        
+    }
+}
+for (const iterator of object) {
+    
+}
+array.forEach(element => {
+    
+});
+for ( let x = 00; x  < 10; ++) {
+    for (let y = 0; y < 10;y++){
+     for   (let z = 0; z < 10; z++) {
+            console.log(x , y , z)
+}
+}
+}
+let myNum = 10;
+
+for (let index = 0; index <= myNum; index++) {
+    result *= i
+}
+console.log( result)
---
+++ Thuc hanh/index.js
@@ -0,0 +1,57 @@
+let a = 10 ;
+let b = 15 ;
+let answer = true; 
+if ( a < b ) {
+console.log(answer);
+}
+let wrong = false;
+let right = true ;
+if ( a = 10 ) {
+    console.log (right);
+}
+if ( a === '10') {
+    console.log (right)
+}
+let x = 5 ;
+let y = 6 ;
+if (x !==y) {
+    console.log(wrong)
+}
+var a1 = 10;
+var b1 = Number(a)
+var c1 = a * 1;
+console.log( a1 , b1 , c1 );
+
+
+var a2 =10;
+var b2= " ";
+if(a2){
+console.log('a is a truthy');
+}
+if(b2){
+console.log('b is a truthy');
+}
+
+let a3 = "mindx"
+let b3 = "";
+if (a||b) {
+    console.log('true')
+}
+let a4 = "mindX"
+let b4 = " ";
+if (a &&true &&!b) {
+    console.log("True")
+} else {  console.log("false")
+}
+ let a5=-1;
+ let b5=1;
+ if( a5&&b5 ){
+     console.log('true')
+ }
+ var number = prompt('Nhap so');
+ if (number % 3 == 0) {
+    console.log(number + ' chia het cho 3')
+ } else {
+     console.log(number + ' khong chia het cho 3')
+ }
+    
