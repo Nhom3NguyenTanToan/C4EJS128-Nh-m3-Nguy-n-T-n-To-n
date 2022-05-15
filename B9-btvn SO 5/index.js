c5b1f4d790dc94d5154902f68a687c9523d02cac
Nhom3NguyenTanToan <nguyentoan3042003@gmail.com>
2022-05-15 21:21:35 +0700

    .

--- B9-btvn SO 5/index.js
+++ B9-btvn SO 5/index.js
@@ -1,5 +1,6 @@
 var n = prompt('nhap so giay');
 let gio = ((n-n%3600)/3600);
 phut  = ((n%3600-(n%3600)%(3600/60))/(3600/60));
+giay = Math.floor(n%3600%60);
 // giay = ((n%3600)/60-(n%3600/60)%(3600/60/60)/(3600/60/60));
-  alert( gio + 'gio ' + " " + phut + " phut"  );
+  alert( gio + 'gio ' + " " + phut + " phut"   + giay+ "giay");
